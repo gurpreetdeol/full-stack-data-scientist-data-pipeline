27946006a78b0d843f35c140993250a0f545fac9
Gurpreet Deol <35876492+gurpreetdeol@users.noreply.github.com>
2024-06-19 11:57:46 -0700

    Update my_secret_key.py

--- my_secret_key.py
+++ my_secret_key.py
@@ -1,2 +1,15 @@
-def my_key():
-    return "AIzaSyAUwv2mKHVF1cJlc1QAvtOEVTHmUGFjSms"
+# importing os module for environment variables
+import os
+# importing necessary functions from dotenv library
+from dotenv import load_dotenv, dotenv_values
+
+# loading variables from .env file
+load_dotenv()
+
+
+def my_youtube_key():
+    return os.getenv("YOUTUBE_KEY_ID")
+
+
+def my_github_token():
+    return os.getenv("GITHUB_TOKEN")
